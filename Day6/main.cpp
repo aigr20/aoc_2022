d054195cb021208f5eb281f91ad6d5bfcf1e1fc6
Wilhelm Gren <wilhelmgren@outlook.com>
2023-01-12 01:18:19 +0100

    Solve Day 6 Part 2

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -4,6 +4,41 @@
 #include <string>
 #include <vector>
 
+int uniq_pos(std::ifstream &file, const int uniques)
+{
+    char ch;
+    std::vector<char> char_tracker;
+    int index = 0;
+    std::map<char, int> char_counter;
+    while (file.peek() != EOF && file.peek() != '\n')
+    {
+        file.get(ch);
+        char_counter[ch] += 1;
+        char_tracker.push_back(ch);
+        if (index >= uniques)
+        {
+            bool unique = true;
+            char_counter[char_tracker[0]] -= 1;
+            char_tracker.erase(char_tracker.begin());
+            for (const auto &c : char_tracker)
+            {
+                if (char_counter[c] != 1)
+                {
+                    unique = false;
+                    break;
+                }
+            }
+            if (unique)
+            {
+                return index + 1;
+            }
+        }
+        index += 1;
+    }
+
+    return -1;
+}
+
 int main()
 {
     std::ifstream input;
@@ -11,37 +46,11 @@ int main()
 
     if (input.is_open())
     {
-        char ch;
-        std::vector<char> last_4;
-        int index = 0;
-        std::map<char, int> char_counter;
-        while (input.peek() != EOF && input.peek() != '\n')
-        {
-            input.get(ch);
-            char_counter[ch] += 1;
-            last_4.push_back(ch);
-            if (index >= 4)
-            {
-                bool unique = true;
-                char_counter[last_4[0]] -= 1;
-                last_4.erase(last_4.begin());
-                for (const auto &c : last_4)
-                {
-                    if (char_counter[c] != 1)
-                    {
-                        unique = false;
-                        break;
-                    }
-                }
-                if (unique)
-                {
-                    std::cout << "Part 1: " << index + 1 << std::endl;
-                    break;
-                }
-            }
-            index += 1;
-        }
-        input.close();
+        std::cout << "Part 1: " << uniq_pos(input, 4) << std::endl;
+        input.seekg(0);
+        std::cout << "Part 2: " << uniq_pos(input, 14) << std::endl;
     }
+    input.close();
+
     return 0;
 }
